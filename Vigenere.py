4090047285b12de9dff669d4ce5dac900193fe91
Logan Freeman <lfreeman@gas.usu.edu>
2020-07-19 17:22:05 -0600

    Encoding/Decoding, and testing implemented!

--- Vigenere.py
+++ Vigenere.py
@@ -7,11 +7,39 @@ def encodeVigenere(text, key): #Both inputs should be in string form
 		if(textList[i]<0):
 			#Letter
 			letter = -1*textList[i]
-			shift = -1*keyList[j%len(keyList)
+			shift = -1*keyList[j%len(keyList)]
 			textList[i] = -1*((letter+shift-1)%26)
 			j+=1
 		i+=1
 	return textList
+
+def decodeVigenere(text, key): #Again, both inputs in string form
+	textList = stringToNumList(text)
+	keyList = stringToNumList(key)
+	i=0
+	j=0
+	while i<len(textList):
+		if(textList[i]<0):
+			#Letter
+			letter = -1*textList[i]
+			shift = -1*keyList[j%len(keyList)]
+			textList[i] = (letter-shift+1)
+			if(textList[i]<0):
+				textList[i]+=26
+			textList[i]*=-1
+			j+=1
+		i+=1
+	return textList
+
+def testEncodeDecode(text, key):
+	encodedList = encodeVigenere(text, key)
+	encodedString = numListToString(encodedList)
+	print(encodedList)
+	print("Encoded Vigenere Cipher: " + encodedString)
+	decodedList = decodeVigenere(encodedString, key)
+	decodedString = numListToString(decodedList)
+	print(decodedList)
+	print("Decoded Vigenere Cipher: " + decodedString)
 
 def charToInt(character):
 	num = ord(character)
@@ -20,7 +48,7 @@ def charToInt(character):
 		return -1*(num-64)
 	elif 97 <= num <= 122:
 		#lowercase letter
-		return -1(num-96)
+		return -1*(num-96)
 	else:
 		return num
 
@@ -35,5 +63,7 @@ def numListToString(nList):
 	string = ""
 	for n in nList:
 		if n<0:
-			n=n*-1
-		string.join(chr(n))
+			string+=str(chr(96+n*-1))
+		else:
+			string+=str(chr(n))
+	return string
