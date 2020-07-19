9dd1497f53c4b68348f3d0ae77bc3cb63fce253a
Logan Freeman <lfreeman@gas.usu.edu>
2020-07-19 16:49:00 -0600

    f

--- Vigenere.py
+++ Vigenere.py
@@ -1,21 +1,28 @@
 def encodeVigenere(text, key): #Both inputs should be in string form
 	textList = stringToNumList(text)
 	keyList = stringToNumList(key)
-	for i in range(0, len(textList)):
-		textList[i] = (textList[i]+keyList[i%len(keyList)])%26 - 1
+	i=0
+	j=0
+	while i<len(textList):
+		if(textList[i]<0):
+			#Letter
+			letter = -1*textList[i]
+			shift = -1*keyList[j%len(keyList)
+			textList[i] = -1*((letter+shift-1)%26)
+			j+=1
+		i+=1
 	return textList
 
 def charToInt(character):
 	num = ord(character)
 	if 65 <= num <= 90:
 		#capital letter
-		return num-64
+		return -1*(num-64)
 	elif 97 <= num <= 122:
 		#lowercase letter
-		return num-96
+		return -1(num-96)
 	else:
-		print("not a valid character")
-		return None
+		return num
 
 def stringToNumList(string):
 	charList = list(string)
@@ -23,3 +30,10 @@ def stringToNumList(string):
 	for c in charList:
 		numList.append(charToInt(c))
 	return numList
+
+def numListToString(nList):
+	string = ""
+	for n in nList:
+		if n<0:
+			n=n*-1
+		string.join(chr(n))
