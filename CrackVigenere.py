aa4354cf54d65a9d51014ed7820d99ec28c248c6
Logan Freeman <lfreeman@gas.usu.edu>
2020-07-21 16:05:06 -0600

    framework for cracking

---
+++ CrackVigenere.py
@@ -0,0 +1,5 @@
+import Vigenere
+#First, Kasiski for key-length identification
+#Then, Caesar decryption for possible key lengths
+#Dictionary checks on Caesar decryptions ---METRIC
+#Cleanliness of frequency distribution on kasiski key-length ---METRIC
