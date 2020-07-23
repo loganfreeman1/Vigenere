c9dfda5195ed36dd2142a1c2117eccf6cef2d06e
Logan Freeman <lfreeman@gas.usu.edu>
2020-07-22 20:00:47 -0600

    Kasiski testing

--- Kasiski.py
+++ Kasiski.py
@@ -1,9 +1,31 @@
 import csv
-import Vigenere
+import EncodeDecode
 #First, Kasiski for key-length identification
 #Then, Caesar decryption for possible key lengths
 #Dictionary checks on Caesar decryptions ---METRIC
 #Cleanliness of frequency distribution on kasiski key-length ---METRIC
+factorFrequency = { #Frequency of factors of differences, done from an analysis of 200,000 random letters
+	2:0,
+	3:0.083566881,
+	4:0.0625754,
+	5:0.05037445,
+	6:0.041757921,
+	7:0.035491564,
+	8:0.03125522,
+	9:0.027926001,
+	10:0.024712782,
+	11:0.02259229,
+	12:0.020998441,
+	13:0.019601793,
+	14:0.017722582,
+	15:0.016325934,
+	16:0.01562993,
+	17:0.014604484,
+	18:0.013989681,
+	19:0.013112716,
+	20:0.012354071
+}
+
 def kasiski(string,minSize=3, maxSize=6):
 	#Takes string, returns structure containing frequency list for potential key lengths.
 	#NOTE: minSize is the minimum Key size to be considered, maxSize is the maximum key size to be considered.
@@ -34,23 +56,37 @@ def kasiski(string,minSize=3, maxSize=6):
 		for i in range(len(instances)-1):
 			spaceList.append(instances[i+1]-instances[i])
 	#Now, spaceList contains every space between instances of repeated substrings. Now, have to make frequency list of key lengths
+	totalFactors=0
 	for num in spaceList:
 		for i in range(minSize, maxSize+1):
 			if num%i==0:
 				if i in frequencies:
 					frequencies[i]+=1
+					totalFactors+=1
 				else:
 					frequencies[i]=1
+					totalFactors+=1
+
+	#Now, do an irregularity analysis, to find the likely key length(s)
 	print(frequencies)
-	csvDict(frequencies)
+	differences = {}
+	for i in range(minSize, maxSize+1):
+		if i in frequencies:
+			expected = (factorFrequency[i]*totalFactors)
+			actual = (frequencies[i])
+			differences[i]=actual-expected
+			print("length "+str(i))
+			print("difference "+str(actual-expected))
+
+	possibleKeyLengths = []
+	sortedDict = sorted(differences, key=differences.get, reverse=True)
+	return sortedDict[:3]
 
 def csvDict(mydict):
 	with open('dict.csv', 'w') as csv_file:
 		writer = csv.writer(csv_file)
 		for key, value in mydict.items():
 			writer.writerow([key, value])
-def crack():
 	#Analyse kasiski for deviation from normal (random) to score probable key-length using R^2
 	#Frequency-analyse most deviant kasiski shifts for normality R^2
 	#TODO FUTURE: CHECK FOR INTELLIGIBLE ENGLISH to score
-	print(8)
